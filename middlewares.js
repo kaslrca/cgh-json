fe673c99c7a51e4fb9f375dde625455ccb06184c
kaslrca <kaslrca@gmail.com>
2021-02-15 16:34:56 +0800

    adjust

--- middlewares.js
+++ middlewares.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
   console.log(body);
   if (req.method === "POST") {
     // If the method is a POST echo back the name from request body
-    res.json({"dataCnt":"1","responseJSON":[{"DIVISIONNO":"001","DRNO":"01967","SEEDATE":"20210223","CREATEUSERNO":"shihfeng","CHARTNO":"0016521013","ROOMNO":"AB1","TIMEDIVISIONNO":"3","DEBUGMSG":"成功"}],"message":"成功筆數1","result":"N","successCnt":"1"});
+    res.json({"dataCnt":"1","responseJSON":[{"DIVISIONNO":"001","DRNO":"05703","SEEDATE":"20210102","CREATEUSERNO":"01967","CHARTNO":"0016521011","ROOMNO":"809","TIMEDIVISIONNO":1,"DEBUGMSG":"成功"}] , "message":"成功筆數1","result":"N","successCnt":"1"});
   }else{
 	//Not a post request. Let db.json handle it
     next();
