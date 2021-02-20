16ef2a2aced5c49a2c89d92271458119263f0888
Huang Shih Feng <kaslrca@dentall.io>
2021-02-20 17:56:19 +0800

    adjust

--- middlewares.js
+++ middlewares.js
@@ -4,7 +4,8 @@ module.exports = (req, res, next) => {
   console.log(body);
   if (req.method === "POST") {
     // If the method is a POST echo back the name from request body
-    res.json({"dataCnt":"1","responseJSON":[{"DIVISIONNO":"001","DRNO":"05703","SEEDATE":"20210102","CREATEUSERNO":"01967","CHARTNO":"0016521011","ROOMNO":"809","TIMEDIVISIONNO":1,"DEBUGMSG":"成功"}] , "message":"成功筆數1","result":"N","successCnt":"1"});
+    res.json({"dataCnt":"1","responseJSON":[{"DIVISIONNO":"001","DRNO":"08905","SEEDATE":"20210129","CREATEUSERNO":"admin","CHARTNO":"0016521010","ROOMNO":"820","TIMEDIVISIONNO":2,"DEBUGMSG":"重複掛號或重複取號[1],請查詢後再掛號"}] , "message":"成功筆數1","result":"N","successCnt":"1"});
+    // res.json({"responseJSON":[] , "message":"", "result":"Y", "failCnt":"0"});
   }else{
 	//Not a post request. Let db.json handle it
     next();
